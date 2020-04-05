bfcd103dbe875cd97a47aa5582f033faa6b51468
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-05 18:14:30 +0500

    test to searchBy added

--- src/main/java/Manager/ProductMananger.java
+++ src/main/java/Manager/ProductMananger.java
@@ -1,7 +1,9 @@
 package Manager;
 
 import repository.ProductRepository;
+import ru.netology.domain.Book;
 import ru.netology.domain.Product;
+import ru.netology.domain.Smartphone;
 
 public class ProductMananger {
 
@@ -16,35 +18,39 @@ public class ProductMananger {
     }
 
     public Product[] searchBy(String text) {
-            Product[] result = new Product[0];
-            for (Product product: repository.getAll()) {
-                if (matches(product, text)) {
-                    Product[] tmp = new Product[result.length + 1];
-                    System.arraycopy(product, 0, tmp, 0, result.length);
-                    tmp[tmp.length - 1] = product;
-                    result = tmp;
-                }
+        Product[] result = new Product[0];
+        for (Product product : repository.getAll()) {
+            if (matches(product, text)) {
+                Product[] tmp = new Product[result.length + 1];
+                System.arraycopy(product, 0, tmp, 0, result.length);
+                tmp[tmp.length - 1] = product;
+                result = tmp;
             }
-            return result;
         }
+        return result;
+    }
 
     public boolean matches(Product product, String search) {
+        if (product instanceof Book) {
+            Book book = (Book) product;
+            if (book.getName().equalsIgnoreCase(search)) {
+                return true;
+            }
+            if (book.getAuthor().equalsIgnoreCase(search)) {
+                return true;
+            }
 
-
-        // ваш код
+            if (product instanceof Smartphone) {
+                Smartphone smartphone = (Smartphone) product;
+                if (smartphone.getName().equalsIgnoreCase(search)) {
+                    return true;
+                }
+                if (smartphone.getProducer().equalsIgnoreCase(search)) {
+                    return true;
+                }
+                return false;
+            }
+        }
         return false;
     }
-
-    }
-
-
-//        if (product instanceof Book) {
-//        Book book = (Book) product;
-//        if (book.getName().equalsIgnoreCase(search)) {
-//        return true;
-//        }
-//        if (book.getAuthor().equalsIgnoreCase(search)) {
-//        return true;
-//        }
-//        return false;
-//        }
+}
--- src/test/java/Manager/ProductManangerTest.java
+++ src/test/java/Manager/ProductManangerTest.java
@@ -1,12 +1,40 @@
 package Manager;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import repository.ProductRepository;
+import ru.netology.domain.Product;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class ProductManangerTest {
+    @Mock
+    private ProductRepository repository;
+    @InjectMocks
+    private ProductMananger mananger;
 
     @Test
-    void searchBy() {
+    void shouldFindIfMatchesQuery() {
+        String text = "Федиатрия";
+        Product[] returned = new Product[]{};
+        doReturn(returned).when(repository).findAll();
+        doNothing().when(mananger.searchBy(text));
+
+        mananger.searchBy("Федиатрия");
+        Product[] expected = new Product[]{};
+        Product[] actual = repository.getAll();
+        assertArrayEquals(expected, actual);
+
+        verify(mananger).searchBy(text);
     }
 }
+//E.g. thenReturn() may be missing.
+//Examples of correct stubbing:
+//    when(mock.isOk()).thenReturn(true);
+//    when(mock.isOk()).thenThrow(exception);
+//    doThrow(exception).when(mock).someVoidMethod();
