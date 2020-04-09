135b8c829c7c7c68e0105408bc859be09b2ebc1b
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-10 01:27:29 +0500

    changes done

--- src/test/java/Manager/ProductManagerTest.java
+++ src/test/java/Manager/ProductManagerTest.java
@@ -19,26 +19,26 @@ import static org.mockito.Mockito.*;
 
 //@ExtendWith(MockitoExtension.class)
 class ProductManagerTest {
-   // @Mock
+    // @Mock
     private ProductRepository repository = new ProductRepository();
-   // @InjectMocks
+    // @InjectMocks
     private ProductManager manager = new ProductManager(repository);
-    private Book[] book;
+    private Product[] product = new Book[]{};
     private Smartphone[] smartphone;
     private Book first = new Book(1, "Faust", 300, "Johann Wolfgang von Goethe");
     private Book second = new Book(2, "The Hobbit", 250, "John Ronald Reuel Tolkien");
     private Book third = new Book(3, "Доктор Живаго", 300, "Борис Пастернак");
 
-   // @BeforeEach
+    // @BeforeEach
     @Test
-    public void Set(){
+    public void Set() {
         manager.add(first);
         manager.add(second);
         manager.add(third);
     }
 
-@Test
-    void shouldFindIfMatchesQuery(){
+    @Test
+    void shouldFindIfMatchesQuery() {
         String text = "The Hobbit";
 
         manager.add(first);
@@ -47,31 +47,10 @@ class ProductManagerTest {
 
         manager.searchBy(text);
 
-        Product[] expected = new Product[]{first};
+        Product[] expected = new Product[]{second};
         Product[] actual = repository.getAll();
 
         assertArrayEquals(expected, actual);
 
+    }
 }
-
-   // @Test
-    //void shouldFindIfMatchesQuery() {
-      //  String text = "Faust";
-       // Product[] returned = new Product[]{};
-       // when(repository.findAll()).thenReturn(returned);
-       // when()
-       // doNothing().when(manager.searchBy(text));
-
-       // manager.searchBy("Faust");
-       // Product[] expected = new Product[]{};
-       // Product[] actual = repository.findAll();
-       // assertArrayEquals(expected, actual);
-
-        //verify(manager).searchBy(text);
-    //}
-}
-//E.g. thenReturn() may be missing.
-//Examples of correct stubbing:
-//    when(mock.isOk()).thenReturn(true);
-//    when(mock.isOk()).thenThrow(exception);
-//    doThrow(exception).when(mock).someVoidMethod();
