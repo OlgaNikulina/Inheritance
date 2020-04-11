3d853a4f130ad5011bb643933dca92bbbaebfddd
OlgaNikulina <nikulinaov@inbox.ru>
2020-04-11 13:01:46 +0500

    tests added

--- src/main/java/ru/netology/ProductRepository.java
+++ src/main/java/ru/netology/ProductRepository.java
@@ -1,41 +1,44 @@
 package ru.netology;
 
 public class ProductRepository {
-        private Product[] items = new Product[0];
+    private Book first = new Book(1, "Faust", 300, "Johann Wolfgang von Goethe");
+    private Book second = new Book(2, "The Hobbit", 250, "John Ronald Reuel Tolkien");
 
-        public void save(Product item) {
-            int length = items.length + 1;
-            Product[] tmp = new Product[length];
-            System.arraycopy(items, 0, tmp, 0, items.length);
-            int lastIndex = tmp.length - 1;
-            tmp[lastIndex] = item;
-            items = tmp;
-        }
+    private Product[] items = new Product[]{};
 
-        public Product[] getAll() {
-            Product[] products = new Product[0];
-            Product[] result = new Product[products.length];
-            for (int i = 0; i < result.length; i++) {
-                int index = products.length - i;
-                result[i] = products[index];
-            }
-            return result;
-        }
-
-        public Product[] findAll() {
-            return items;
-        }
-
-        public void removeById(int id) {
-            int length = items.length - 1;
-            Product[] tmp = new Product[length];
-            int index = 0;
-            for (Product item : items) {
-                if (item.getId() != id) {
-                    tmp[index] = item;
-                    index++;
-                }
-            }
-            items = tmp;
-        }
+    public void save(Product item) {
+        int length = items.length + 1;
+        Product[] tmp = new Product[length];
+        System.arraycopy(items, 0, tmp, 0, items.length);
+        int lastIndex = tmp.length - 1;
+        tmp[lastIndex] = item;
+        items = tmp;
     }
+
+    public Product[] getAll() {
+        int length = items.length + 1;
+        Product[] result = new Product[items.length];
+        for (int i = 0; i < result.length; i++) {
+            int index = items.length - 1;
+            result[i] = items[index];
+        }
+        return result;
+    }
+
+    public Product[] findAll() {
+        return items;
+    }
+
+    public void removeById(int id) {
+        int length = items.length - 1;
+        Product[] tmp = new Product[length];
+        int index = 0;
+        for (Product item : items) {
+            if (item.getId() != id) {
+                tmp[index] = item;
+                index++;
+            }
+        }
+        items = tmp;
+    }
+}
--- src/test/java/ru/netology/ProductRepositoryTest.java
+++ src/test/java/ru/netology/ProductRepositoryTest.java
@@ -6,6 +6,56 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
 
+
+    @Test
+    public void shouldGetTwoBooks() {
+        ProductManager manager = new ProductManager(repository);
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        repository.getAll();
+
+        Product[] expected = new Product[]{first, second, third};
+        Product[] actual = repository.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldRemoveById() {
+        int idToRemove = 2;
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        repository.removeById(idToRemove);
+
+        Product[] expected = new Product[]{first, third};
+        Product[] actual = repository.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotRemoveIfNotExists() {
+        int idToRemove = 4;
+
+        manager.add(first);
+        manager.add(second);
+        manager.add(third);
+
+        repository.removeById(idToRemove);
+
+        Product[] expected = new Product[]{first, second, third};
+        Product[] actual = repository.findAll();
+
+        assertArrayEquals(expected, actual);
+    }
+}
+
     @Test
     void save() {
     }
